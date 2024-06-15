08502ba5fa84aa75ef5ef70a9bfeb298d3a1744c
Gabriel Feippe <gabriel@feippe.com>
2024-06-15 20:44:59 -0300

    Step 3

---
+++ scripts/getDates.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  putYearInFooter();
+  putLastModificationInFooter();
+});
+
+function putYearInFooter() {
+  const date = new Date();
+  let year = date.getFullYear();
+  document.getElementById("footerYear").innerHTML = year;
+}
+
+function putLastModificationInFooter() {
+  let lastModification = "01/01/1970 12:00:00";
+  const date = new Date();
+  lastModification = new Intl.DateTimeFormat('es-UY', { day: '2-digit', month: '2-digit', year: 'numeric', hour:'2-digit', minute:'2-digit', second:'2-digit' });
+  lastModification = lastModification.format(date);
+  lastModification = `Last Modification: ${lastModification}`;
+  document.getElementById("lastModified").innerHTML = lastModification;
+}
+
+
