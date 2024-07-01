6437f73d50e045a63eeaa48511a6b77f47016f93
Gabriel Feippe <gabriel@feippe.com>
2024-07-01 20:55:53 -0300

    lazy load page

---
+++ week03/scripts/lazyload-scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    putYearInFooter();
+    putLastModificationInFooter();
+  });
+  
+  function putYearInFooter() {
+    const date = new Date();
+    let year = date.getFullYear();
+    document.getElementById("footerYear").innerHTML = year;
+  }
+  
+  function putLastModificationInFooter() {
+    if (Date.parse(document.lastModified) != 0){
+      let lastModification = `Last Modification: ${document.lastModified}`;
+      document.getElementById("lastModified").innerHTML = lastModification;
+    }
+  }
+  
