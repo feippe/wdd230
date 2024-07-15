87f84fe458e4324e9f313e21fcf53f45ab1c3caf
Gabriel Feippe <gabriel@feippe.com>
2024-07-15 19:03:18 -0300

    W05 JSON LINKS improve

--- scripts/links.js
+++ scripts/links.js
@@ -16,6 +16,9 @@ function displayLinks(data){
             let a = document.createElement('a');
             a.textContent = link.title;
             a.setAttribute('href',link.url);
+            if(link.target!=undefined){
+                a.setAttribute('target',link.target);
+            }
             li.append(a);
         });
         ul.append(li);
